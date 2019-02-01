dcd7d4db96f827355beb6d2cb9a57a48d290326f
Fabio <fa.espinosa10@uniandes.edu.co>
2019-02-01 08:23:05 +0100

    add tests

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+const assert = require('assert').strict;
 module.exports = (object1, object2) => {
-    const returned_object = {};
+    // If either object1 or object2 is object and other is array, throw eeror
+    if (Array.isArray(object1) && !Array.isArray(object2)) {
+        throw 'Comparing incompatible types';
+    }
+    if (Array.isArray(object2) && !Array.isArray(object1)) {
+        throw 'Comparing incompatible types';
+    }
+
+    // If both are arrays it is ok, but we default to object
+    let returned_object = {};
+    if (Array.isArray(object1)) {
+        returned_object = [];
+    }
+
+    // We will now compare each property, if it changed, we add it to returned_object
+
     for (const [key, val] of Object.entries(object2)) {
-        if (object1[key] !== val) {
-            // This means the property has changed:
-            returned_object[key] = val;
+        const possible_old_value = object1[key];
+        const possible_new_value = val;
+        try {
+            assert.deepStrictEqual(possible_old_value, possible_new_value);
+        } catch (e) {
+            if (Array.isArray(returned_object)) {
+                returned_object.push(val);
+            } else {
+                returned_object[key] = val;
+            }
         }
     }
     return returned_object;
 };
+
+module.exports(['DAQ', 'DT'], ['DAQ', 'DT', 'TCDS']);
--- test.js
+++ test.js
@@ -110,81 +110,64 @@ test('with same number of components, but different', t => {
         getObjectWithAttributesThatChanged(
             {
                 components: ['DAQ', 'DT', 'TCDS'],
-                l1_hlt_mode_stripped: null,
-                l1_key: null,
-                duration: 339,
-                b_field: 0.019,
-                tier0_transfer: true,
-                l1_triggers_counter: null,
-                hlt_physics_throughput: null,
-                init_lumi: 0,
-                last_update: '2019-01-18T21:28:23Z',
-                delivered_lumi: 0,
-                recorded_lumi: 0,
-                cmssw_version: 'CMSSW_10_1_2_patch2',
-                energy: null,
-                end_lumi: 0,
-                hlt_physics_rate: null,
-                fill_number: null,
-                l1_hlt_mode: null,
-                end_time: '2019-01-18T19:59:01Z',
-                trigger_mode: 'MANUAL',
-                l1_key_stripped: null,
-                fill_type_party2: null,
-                fill_type_party1: null,
-                initial_prescale_index: null,
-                sequence: 'PRIVATE-GLOBAL-DT',
-                start_time: '2019-01-18T19:53:22Z',
-                hlt_physics_size: 0.013,
-                fill_type_runtime: null,
-                hlt_key: '/minidaq/2017/Basic/HLT/V4',
-                clock_type: 'LOCAL',
-                l1_rate: null,
-                l1_menu: null,
-                run_number: 328025,
-                stable_beam: false,
-                hlt_physics_counter: 12282
+                l1_hlt_mode_stripped: null
             },
             {
                 components: ['DAQ', 'ECAL', 'TCDS'],
-                l1_hlt_mode_stripped: null,
-                l1_key: null,
-                duration: 339,
-                b_field: 0.019,
-                tier0_transfer: true,
-                l1_triggers_counter: null,
-                hlt_physics_throughput: null,
-                init_lumi: 0,
-                last_update: '2019-01-18T21:28:23Z',
-                delivered_lumi: 0,
-                recorded_lumi: 0,
-                cmssw_version: 'CMSSW_10_1_2_patch2',
-                energy: null,
-                end_lumi: 0,
-                hlt_physics_rate: null,
-                fill_number: null,
-                l1_hlt_mode: null,
-                end_time: '2019-01-18T19:59:01Z',
-                trigger_mode: 'MANUAL',
-                l1_key_stripped: null,
-                fill_type_party2: null,
-                fill_type_party1: null,
-                initial_prescale_index: null,
-                sequence: 'PRIVATE-GLOBAL-DT',
-                start_time: '2019-01-18T19:53:22Z',
-                hlt_physics_size: 0.013,
-                fill_type_runtime: null,
-                hlt_key: '/minidaq/2017/Basic/HLT/V4',
-                clock_type: 'LOCAL',
-                l1_rate: null,
-                l1_menu: null,
-                run_number: 328025,
-                stable_beam: false,
-                hlt_physics_counter: 12282
+                l1_hlt_mode_stripped: null
             }
         ),
         {
             components: ['DAQ', 'ECAL', 'TCDS']
         }
+    );
+});
+
+test('with nested object', t => {
+    t.deepEqual(
+        getObjectWithAttributesThatChanged(
+            {
+                components: ['DAQ', 'DT', 'TCDS'],
+                l1_hlt_mode_stripped: null,
+                inside_object: {
+                    inside_property: 1,
+                    inside_property: 2
+                }
+            },
+            {
+                components: ['DAQ', 'DT', 'TCDS'],
+                l1_hlt_mode_stripped: null,
+                inside_object: {
+                    inside_property: 1,
+                    inside_property: 3
+                }
+            }
+        ),
+        {
+            inside_object: {
+                inside_property: 1,
+                inside_property: 3
+            }
+        }
+    );
+});
+
+test('with array that doesnt change', t => {
+    t.deepEqual(
+        getObjectWithAttributesThatChanged(
+            ['DAQ', 'DT', 'TCDS'],
+            ['DAQ', 'DT', 'TCDS']
+        ),
+        []
+    );
+});
+
+test('with array that changes', t => {
+    t.deepEqual(
+        getObjectWithAttributesThatChanged(
+            ['DAQ', 'DT'],
+            ['DAQ', 'DT', 'TCDS']
+        ),
+        ['TCDS']
     );
 });
